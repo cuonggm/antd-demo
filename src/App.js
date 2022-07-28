7e12cd943cabb4f0ecd7c6d2ca708a89ca36ea8e
CuongGM <cuonggm48@gmail.com>
2022-07-28 23:49:55 +0700

    Edit header

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
 import "./antd-theme/antd-customized.css";
 import { BrowserRouter, Link, Route, Switch } from "react-router-dom";
-import { Layout } from "antd";
-import { ChromeOutlined } from "@ant-design/icons";
+import { Layout, Menu } from "antd";
 import Login from "./pages/Login/Login";
+import styles from "./App.module.css"
 
 // Do not import. Just get from Layout
 const { Content, Header, Footer } = Layout;
 
 function App() {
+  const headerBackgroundColor = "#5DAA68";
+
   return (
     <BrowserRouter>
       <Layout style={{ height: "100vh" }}>
-        <Header style={{ backgroundColor: "#5DAA68", color: "#FAF1CF" }}>
-          <Link to="/" style={{ color: "white", fontSize: "18pt" }}>
-            <ChromeOutlined style={{ marginRight: "2px" }} />
-            Home
-          </Link>
+        <Header style={{ backgroundColor: headerBackgroundColor, height: "70px" }}>
+          <Link to="/login" className={styles.headerItem}>Login</Link>
         </Header>
         <Layout>
           {/* <Sider
